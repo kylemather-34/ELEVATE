69c87fff9ae53761359725005d211a418e363978
Eric Garcia <cubingguy714@gmail.com>
2026-02-24 08:51:16 -0500

    Integrated pipeline to Core

--- elevate/src/core/ElevateCore.ts
+++ elevate/src/core/ElevateCore.ts
@@ -1,16 +1,33 @@
 import * as vscode from 'vscode';
 import { Logger } from '../Logger';
 import { ExtensionController } from '../extension/ExtensionController';
+import { Pipeline } from '../pipeline/Pipeline';
+import { ElevateContext } from './ElevateContext';
+import * as stage from '../pipeline/Stage';
 
+const stages = [
+    new stage.ContextBuilderStage(),
+    new stage.SanitizationStage(),
+    new stage.ParseStage(),
+    new stage.PromptBuilderStage(),
+    new stage.OllamaStage()
+];
 
 export class ElevateCore {
     // Members
-    //DebugLogger: Logger;
+    DebugLogger: Logger;
     ExtensionController: ExtensionController;
+    Pipeline: Pipeline;
+    // Job Queue
 
     constructor() {
-        //this.DebugLogger = logger;
+        this.DebugLogger = new Logger();
         this.ExtensionController = new ExtensionController();
+        this.Pipeline = new Pipeline(stages, this.DebugLogger);
+    }
+
+    executePipeline(ctx: ElevateContext) {
+        this.Pipeline.execute(ctx);
     }
 
 }
