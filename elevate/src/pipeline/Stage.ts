7b9b02a25a002c435dea29b62be7c7013de527f7
Eric Garcia <cubingguy714@gmail.com>
2026-02-24 08:50:59 -0500

    Defined more stage classes

--- elevate/src/pipeline/Stage.ts
+++ elevate/src/pipeline/Stage.ts
@@ -5,10 +5,38 @@ export interface Stage {
     run(ctx: ElevateContext): Promise<void>;
 }
 
-class ParseStage implements Stage {
+export class ContextBuilderStage implements Stage {
+    name = "Context Builder Stage";
+    async run(ctx: ElevateContext): Promise<void> {
+
+    }
+}
+
+export class SanitizationStage implements Stage {
+    name = "Sanitization Stage";
+    async run(ctx: ElevateContext): Promise<void> {
+
+    }
+}
+
+export class ParseStage implements Stage {
+    name = "Parse Stage";
     async run(ctx: ElevateContext): Promise<void> {
         // run parse stage here
         // ctx.parsed = parse()
     }
 }
 
+export class PromptBuilderStage implements Stage {
+    name = "Prompt Builder Stage";
+    async run(ctx: ElevateContext): Promise<void> {
+
+    }
+}
+
+export class OllamaStage implements Stage {
+    name = "Ollama Stage";
+    async run(ctx: ElevateContext): Promise<void> {
+
+    }
+}
