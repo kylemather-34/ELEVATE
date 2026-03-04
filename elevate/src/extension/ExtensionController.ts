b0386ec1fe26946e732ce84dc79849155d31fd0f
RyanL4004 <rlang5@unh.newhaven.edu>
2026-03-04 13:09:07 -0500

    Implemented the "Open File Listener" to trigger analysis when a file is opened.

--- elevate/src/extension/ExtensionController.ts
+++ elevate/src/extension/ExtensionController.ts
@@ -1,6 +1,34 @@
+import * as vscode from 'vscode';
+import { ElevateContext } from '../core/ElevateContext';
+import { Pipeline } from '../pipeline/Pipeline';
+import { Logger } from '../Logger';
+import * as stage from '../pipeline/Stage';
 
 export class ExtensionController {
 
     constructor(){}
 
+public activateOpenFileListener(context: vscode.ExtensionContext): void {
+        const openFileListener = vscode.workspace.onDidOpenTextDocument(
+            async (document: vscode.TextDocument) => {
+                if (document.uri.scheme !== 'file') return;
+
+                const logger = new Logger();
+                const pipeline = new Pipeline(
+                    [
+                        new stage.SanitizationStage(),
+                        new stage.ParseStage(),
+                        new stage.PromptBuilderStage(),
+                        new stage.OllamaStage()
+                    ],
+                    logger
+                );
+
+                const ctx = new ElevateContext(document);
+                await pipeline.execute(ctx);
+            }
+        );
+
+        context.subscriptions.push(openFileListener);
+    }
 }
